b9d28a512ff93434dbbeb857ab96c9bd8bd0dd03
saneki <fake.saneki@gmail.com>
2013-10-24 15:16:53 -0500

    Offline mode support.

--- src/main/java/net/technicpack/launchercore/auth/Profile.java
+++ src/main/java/net/technicpack/launchercore/auth/Profile.java
@@ -23,6 +23,11 @@ public class Profile {
 	private String id;
 	private String name;
 
+	public Profile(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
 	public String getId() {
 		return id;
 	}
