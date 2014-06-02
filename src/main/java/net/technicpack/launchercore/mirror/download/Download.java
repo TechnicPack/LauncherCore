9222b2b91a14100134e3e7acd3a4f07c7af27208
Dylan Page <Dylandcpage@gmail.com>
2014-06-02 19:58:16 -0400

    Fix downloading from redirected URLs

--- src/main/java/net/technicpack/launchercore/mirror/download/Download.java
+++ src/main/java/net/technicpack/launchercore/mirror/download/Download.java
@@ -70,8 +70,18 @@ public class Download implements Runnable {
             int responseFamily = response / 100;
 
             if (responseFamily == 3) {
-                throw new DownloadException("The server issued a redirect response which Technic failed to follow.");
-            } else if (responseFamily != 2) {
+                //throw new DownloadException("The server issued a redirect response which Technic failed to follow.");
+
+                String redirectionLocation = conn.getHeaderField("Location");
+                System.out.println("Redirection URL found. New URL: " + redirectionLocation);
+                URL redirectURL = new URL(redirectionLocation);
+                conn = Utils.openHttpConnection(redirectURL);
+
+                response = conn.getResponseCode();
+                responseFamily = response / 100;
+            }
+
+            if (responseFamily != 2) {
                 throw new DownloadException("The server issued a " + response + " response code.");
             }
 
